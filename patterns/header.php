dce8111ae6353ed093ae4acaeb3a85ccbc99cc08
Maksym Marko <markomaksym@gmail.com>
2023-07-22 10:39:17 +0300

    templates/index.html.

--- patterns/header.php
+++ patterns/header.php
@@ -10,8 +10,8 @@
  */
 
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|small","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small"}}},"backgroundColor":"main-dark","textColor":"white","className":"has-background-color","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-background-color has-white-color has-main-dark-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"var:preset|spacing|medium","right":"var:preset|spacing|small","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|small"},"blockGap":"0","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"main-dark","textColor":"white","className":"has-background-color","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull has-background-color has-white-color has-main-dark-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"wrap"}} -->
 <div class="wp-block-group alignwide"><!-- wp:group {"layout":{"type":"flex"}} -->
 <div class="wp-block-group"><!-- wp:site-logo /--></div>
 <!-- /wp:group -->
