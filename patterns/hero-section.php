8e1ce68364479b455252afe517cab4adaf8af731
Maksym Marko <markomaksym@gmail.com>
2023-07-24 19:15:38 +0300

    Frontend page started.

---
+++ patterns/hero-section.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: Hero Section
+ * Slug: hero-section
+ * Description: Hero Section for Home page
+ * Categories: header
+ * Keywords: hero banner, hero section
+ * Inserter: true
+ */
+
+?>
