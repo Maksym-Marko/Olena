8c74af52c3d24c8bf584580b842a903936b90d8f
Maksym Marko <markomaksym@gmail.com>
2023-08-04 11:56:21 +0300

    Animated section (vertical).

--- patterns/animated-vertical.php
+++ patterns/animated-vertical.php
@@ -11,111 +11,90 @@
 
 ?>
 <!-- wp:group {"tagName":"section","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|xx-large","bottom":"var:preset|spacing|huge"}}},"backgroundColor":"main-light","layout":{"type":"constrained"}} -->
-<section id="animated-vertical" class="wp-block-group has-main-light-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xx-large);padding-bottom:var(--wp--preset--spacing--huge)"><!-- wp:olena/responsive-box {"unique_class":"mx-responsive-box-H3aW2dYZZgo5"} -->
-<div data-breakpoint="desktop" class="mx-responsive-box mx-responsive-box-H3aW2dYZZgo5">
-<div class="wp-block-olena-responsive-box">
-<style>
-@media (max-width: 991.5px) {
-.mx-responsive-box.mx-responsive-box-H3aW2dYZZgo5 {
-display: none !important;
-}
-
-.editor-styles-wrapper .mx-responsive-box.mx-responsive-box-H3aW2dYZZgo5 {
-display: block !important;
-opacity: 0.5;
-}
+<section id="animated-vertical" class="wp-block-group has-main-light-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--xx-large);padding-bottom:var(--wp--preset--spacing--huge)"><!-- wp:olena/responsive-box {"unique_class":"mx-responsive-box-Ii4nBSHRFQvM"} -->
+<div data-breakpoint="desktop" class="mx-responsive-box mx-responsive-box-Ii4nBSHRFQvM"><div class="wp-block-olena-responsive-box"><style>@media (max-width: 991.5px) {
+	.mx-responsive-box.mx-responsive-box-Ii4nBSHRFQvM {
+		display: none !important;
+	}
+	.editor-styles-wrapper .mx-responsive-box.mx-responsive-box-Ii4nBSHRFQvM {
+		display: block !important;
+		opacity: 0.5;
+	}
 }
 </style><!-- wp:olena/animated-section-vertical -->
-<div class="wp-block-olena-animated-section-vertical" data-section-height="4000" data-start-position="top 20%"><!-- wp:group {"style":{"dimensions":{"minHeight":"0vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-olena-animated-section-vertical" data-section-height="2000" data-start-position="top 20%"><!-- wp:group {"style":{"dimensions":{"minHeight":"0vh"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="min-height:0vh"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"45%"} -->
-<div class="wp-block-column" style="flex-basis:45%"><!-- wp:group {"className":"is-style-animation-pointer","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":190,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/animated-img1.jpg" alt="" class="wp-image-190" /></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-columns"><!-- wp:column {"width":"40%"} -->
+<div class="wp-block-column" style="flex-basis:40%"><!-- wp:group {"className":"is-style-animation-pointer","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":40,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_1.png" alt="" class="wp-image-40"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"is-style-animation-pointer","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":206,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/animated-img2.jpg" alt="" class="wp-image-206" /></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":44,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_2.png" alt="" class="wp-image-44"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"is-style-animation-pointer","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":207,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/animated-img3.jpg" alt="" class="wp-image-207" /></figure>
-<!-- /wp:image -->
-</div>
+<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":46,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_3.png" alt="" class="wp-image-46"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"className":"is-style-animation-pointer","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":218,"sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/animated-img4.jpg" alt="" class="wp-image-218" /></figure>
-<!-- /wp:image -->
-</div>
-<!-- /wp:group -->
-</div>
+<div class="wp-block-group is-style-animation-pointer"><!-- wp:image {"id":48,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_4.png" alt="" class="wp-image-48"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
 <!-- /wp:column -->
 
-<!-- wp:column {"width":"55%","layout":{"type":"default"}} -->
-<div class="wp-block-column" style="flex-basis:55%"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"var:preset|spacing|mini","top":"var:preset|spacing|large"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--large);padding-right:0;padding-left:var(--wp--preset--spacing--mini)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0"}}},"className":"is-style-animation-descriptor active-nav","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-descriptor active-nav" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"500"}},"textColor":"main","fontSize":"xx-large"} -->
-<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:500;line-height:1.1">Use no code</h2>
+<!-- wp:column {"width":"60%","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:60%"><!-- wp:group {"style":{"spacing":{"padding":{"right":"0","left":"var:preset|spacing|mini","top":"0"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group" style="padding-top:0;padding-right:0;padding-left:var(--wp--preset--spacing--mini)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"0"}}},"className":"is-style-animation-descriptor active-nav","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-animation-descriptor active-nav" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'NO CODE', 'olena' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size">You need no code skills to create a professional WordPress website. FSE feature allows you build your professional online presence very quickly</p>
-<!-- /wp:paragraph -->
-</div>
+<p class="has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Unlock the power of effortless website creation with <strong>OLENA WordPress Theme</strong>. Embrace the user-friendly FSE feature and build a professional online presence in no time, without the need for any coding skills', 'olena' ) ); ?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0"}}},"className":"is-style-animation-descriptor","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"500"}},"textColor":"main","fontSize":"xx-large"} -->
-<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:500;line-height:1.1">Free Patterns</h2>
+<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'MANY PATTERNS', 'olena' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size">Olena WordPress Theme has 10 patterns to make it easier creating a website and make it faster as well</p>
-<!-- /wp:paragraph -->
-</div>
+<p class="has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Discover a world of design possibilities with <strong>OLENA WordPress Theme</strong>. Choose from 29 prebuilt patterns to effortlessly create a stunning website. Save time and accelerate your website-building journey', 'olena' ) ); ?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"0"}}},"className":"is-style-animation-descriptor","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"500"}},"textColor":"main","fontSize":"xx-large"} -->
-<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:500;line-height:1.1">Get Olena Theme</h2>
+<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'FREE TO USE', 'olena' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size">Simply download and use! You don’t need make any payments or registrations to get and use Olena Theme</p>
-<!-- /wp:paragraph -->
-</div>
+<p class="has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Say hello to freedom! <strong>OLENA Theme</strong> comes completely free of charge. No payments or registrations required – simply download and start using this powerful theme for your website', 'olena' ) ); ?></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0"}}},"className":"is-style-animation-descriptor","layout":{"type":"constrained"}} -->
-<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"500"}},"textColor":"main","fontSize":"xx-large"} -->
-<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:500;line-height:1.1">Cool Animations</h2>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"className":"is-style-animation-descriptor","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-animation-descriptor" style="padding-top:0;padding-bottom:0"><!-- wp:heading {"style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'ANIMATE NOW!', 'olena' ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"fontSize":"x-small"} -->
-<p class="has-x-small-font-size">Olena theme allows you to enable modern animations to make your website more attractive</p>
-<!-- /wp:paragraph -->
-</div>
-<!-- /wp:group -->
-</div>
-<!-- /wp:group -->
-</div>
-<!-- /wp:column -->
-</div>
-<!-- /wp:columns -->
-</div>
-<!-- /wp:group -->
-</div>
-<!-- /wp:olena/animated-section-vertical -->
-</div>
-</div>
-<!-- /wp:olena/responsive-box -->
-</section>
+<p class="has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Add a touch of modernity to your website. <strong>OLENA Theme</strong> offers seamless integration of captivating animations, making your website more attractive and engaging to your audience', 'olena' ) ); ?></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:olena/animated-section-vertical --></div></div>
+<!-- /wp:olena/responsive-box --></section>
 <!-- /wp:group -->
