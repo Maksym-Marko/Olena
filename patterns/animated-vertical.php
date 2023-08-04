e4a8e8b9ea7324c864040d325db3f53d9a46ad32
Maksym Marko <markomaksym@gmail.com>
2023-08-04 12:41:58 +0300

    Animated vertical mobile.

--- patterns/animated-vertical.php
+++ patterns/animated-vertical.php
@@ -96,5 +96,78 @@
 <!-- /wp:columns --></div>
 <!-- /wp:group --></div>
 <!-- /wp:olena/animated-section-vertical --></div></div>
+<!-- /wp:olena/responsive-box --><!-- wp:olena/responsive-box {"unique_class":"mx-responsive-box-0X6lkvGES7K0"} -->
+<div data-breakpoint="tablets-mobile" class="mx-responsive-box mx-responsive-box-0X6lkvGES7K0"><div class="wp-block-olena-responsive-box"><style>@media (min-width: 991.5px) {
+	.mx-responsive-box.mx-responsive-box-0X6lkvGES7K0 {
+		display: none !important;
+	}
+	.editor-styles-wrapper .mx-responsive-box.mx-responsive-box-0X6lkvGES7K0 {
+		display: block !important;
+		opacity: 0.5;
+	}
+}
+</style><!-- wp:olena/animated-box -->
+<div class="wp-block-olena-animated-box" data-animation-type="zoomIn" data-extras=""><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'NO CODE', 'olena' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Unlock the power of effortless website creation with <strong>OLENA WordPress Theme</strong>. Embrace the user-friendly FSE feature and build a professional online presence in no time, without the need for any coding skills', 'olena' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":40,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_1.png" alt="" class="wp-image-40"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:olena/animated-box -->
+
+<!-- wp:olena/animated-box -->
+<div class="wp-block-olena-animated-box" data-animation-type="zoomIn" data-extras=""><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'FREE TO USE', 'olena' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Say hello to freedom! <strong>OLENA Theme</strong> comes completely free of charge. No payments or registrations required – simply download and start using this powerful theme for your website', 'olena' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":46,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_3.png" alt="" class="wp-image-46"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:olena/animated-box -->
+
+<!-- wp:olena/animated-box -->
+<div class="wp-block-olena-animated-box" data-animation-type="zoomIn" data-extras=""><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'MANY PATTERNS', 'olena' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Discover a world of design possibilities with <strong>OLENA WordPress Theme</strong>. Choose from 29 prebuilt patterns to effortlessly create a stunning website. Save time and accelerate your website-building journey', 'olena' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":44,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_2.png" alt="" class="wp-image-44"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:olena/animated-box -->
+
+<!-- wp:olena/animated-box -->
+<div class="wp-block-olena-animated-box" data-animation-type="zoomIn" data-extras=""><!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|small","left":"var:preset|spacing|small","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--small)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"lineHeight":1.1,"fontStyle":"normal","fontWeight":"700"}},"textColor":"main","fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-main-color has-text-color has-xx-large-font-size" style="font-style:normal;font-weight:700;line-height:1.1"><?php echo __( 'ANIMATE NOW!', 'olena' ); ?></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"x-small"} -->
+<p class="has-text-align-center has-x-small-font-size"><?php printf( esc_html__( '%s', 'olena' ), __( 'Add a touch of modernity to your website. <strong>OLENA Theme</strong> offers seamless integration of captivating animations, making your website more attractive and engaging to your audience', 'olena' ) ); ?></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":46,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/sections/animated-vertical/image_3.png" alt="" class="wp-image-46"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:olena/animated-box --></div></div>
 <!-- /wp:olena/responsive-box --></section>
 <!-- /wp:group -->
