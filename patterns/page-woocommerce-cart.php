519da6de4b14dec00e0a9d8bdbfc7c230c2c1c66
Maksym Marko <markomaksym@gmail.com>
2024-03-21 21:46:59 +0200

    WooCommerce. More styles added.

---
+++ patterns/page-woocommerce-cart.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: WooCommerce Cart
+ * Slug: olena/woocommerce-cart
+ * Inserter: no
+ */
+?>
+<!-- wp:woocommerce/page-content-wrapper {"page":"cart"} -->
+<!-- wp:group {"className":"is-style-animation-wrapper","layout":{"type":"constrained"}} -->
+<div class="wp-block-group is-style-animation-wrapper"><!-- wp:olena/floating-box -->
+<div class="wp-block-olena-floating-box" style="right:0%;top:0%" data-orientation="2" data-position-left="-1" data-position-top="0" data-position-right="0" data-position-bottom="-17"><!-- wp:olena/responsive-box {"unique_class":"mx-responsive-box-hGulkjtKHR7H"} -->
+<div data-breakpoint="all" class="mx-responsive-box mx-responsive-box-hGulkjtKHR7H"><div class="wp-block-olena-responsive-box"><style></style><!-- wp:image {"id":67,"width":"448px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/images/animations/bubble_3.svg" alt="" class="wp-image-67" style="width:448px;height:auto"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:olena/responsive-box --></div>
+<!-- /wp:olena/floating-box -->
+
+<!-- wp:woocommerce/store-notices /-->
+
+<!-- wp:post-title {"level":1,"align":"wide"} /-->
+
+<!-- wp:post-content {"align":"wide"} /--></div>
+<!-- /wp:group -->
+<!-- /wp:woocommerce/page-content-wrapper -->
